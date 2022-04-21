e6f4c29c3f4e2d45f8cc3da33cf83e5e53c3b6fa
loyalty <jrodriguezneira@outlook.com>
2022-04-21 23:20:08 +1000

    r

--- loy/extractors/offer-to-file.php
+++ loy/extractors/offer-to-file.php
@@ -209,7 +209,7 @@ $sheet = $spreadsheet->getActiveSheet();
     //Counter to set the starting point for new product $tier[2] needs to be divided by 2 to get the number of tiers   
     $tot_tiers =  $tot_tiers + 2 + $tier[2]/2;    
     }
-    echo $url;
+
 // Write an .xlsx file  
 $writer = new Xlsx($spreadsheet); 
 // Save .xlsx file to the files directory 
