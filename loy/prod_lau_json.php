e526888b2f7bc98e3a6cab20e427b2fba0c44eef
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2022-07-04 02:59:38 +1000

    Last launches

---
+++ loy/prod_lau_json.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-Type: application/json');   
+include 'data/db_connection.php'; 
+$sql=" select sku,name,min(date(date_report)) as launch_date
+from products
+group by sku,name
+order by launch_date desc";
+$response = array();
+$posts = array();
+$result=mysqli_query($con,$sql);
+$launch_date="";
+$i=1;
+while($row=mysqli_fetch_array($result)) 
+{ 
+  if($launch_date==$row['launch_date']){
+    $names.= $row['sku']. " " . $row['name']."<br>";
+
+    if($i == mysqli_num_rows($result)){
+      $posts['name']=$names;
+      array_push($response, $posts);
+    }
+  }
+
+  if($launch_date!=$row['launch_date']){
+  
+    if($launch_date!=""){ 
+      $posts['name']=$names;
+      array_push($response, $posts);
+      $names="";
+  }
+
+  $launch_date=$row['launch_date'];
+  $posts['launch_date']=$row['launch_date'];
+  $names.= $row['sku']. " " . $row['name']."<br>";
+
+  }
+$i++;
+} 
+echo "{\"data\": ".json_encode($response)."}";
+//$file = 'file.json';
+//file_put_contents($file, $json);
+?>
