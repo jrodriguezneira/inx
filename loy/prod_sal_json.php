00d37fac690411e5820672290cd60e96627fd15e
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2022-08-09 00:24:10 +1000

    Sales history automation

--- loy/prod_sal_json.php
+++ loy/prod_sal_json.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');   
 include 'data/db_connection.php'; 
-$sql="SELECT sku,`name`,day_sales FROM product_sales ;";
+$sql="SELECT sku,`name`,day_sales_loy FROM product_sales where date_report=DATE_SUB(CURDATE(), INTERVAL 2 DAY);";
 $response = array();
 $posts = array();
 $result=mysqli_query($con,$sql);
@@ -10,10 +10,10 @@ while($row=mysqli_fetch_array($result))
   
   $posts['sku']=$row['sku'];
   $posts['name']=$row['name'];
-  $posts['day_sales']=$row['day_sales'];
+  $posts['day_sales']=$row['day_sales_loy'];
 
-  //Get sales from previous day
-  $sql1="SELECT day_sales FROM product_sales  where sku='".$row['sku']."' and date_report=DATE_SUB(CURDATE(), INTERVAL 3 DAY);";
+//   //Get sales from previous day
+  $sql1="SELECT day_sales_loy FROM product_sales  where sku='".$row['sku']."' and date_report=DATE_SUB(CURDATE(), INTERVAL 3 DAY);";
   $result2=mysqli_query($con,$sql1);
     while($sales=mysqli_fetch_array($result2)) 
     { 
@@ -22,8 +22,8 @@ while($row=mysqli_fetch_array($result))
 
   $posts['yesterday_sales']=$sale;
 
-  //Get sales from pre - previous day
-  $sql2="SELECT day_sales FROM product_sales  where sku='".$row['sku']."' and date_report=DATE_SUB(CURDATE(), INTERVAL 4 DAY);";
+//   //Get sales from pre - previous day
+  $sql2="SELECT day_sales_loy FROM product_sales  where sku='".$row['sku']."' and date_report=DATE_SUB(CURDATE(), INTERVAL 4 DAY);";
   $result3=mysqli_query($con,$sql2);
     while($presales=mysqli_fetch_array($result3)) 
     { 
@@ -31,17 +31,17 @@ while($row=mysqli_fetch_array($result))
     }
 
   $posts['pre_sales']=$presale;
-/*
+// /*
     //Get price
-    $sql3="SELECT price FROM products_last  where sku='".$row['sku']."' and segment='LOYALTY_CON' limit 1";
-    $result4=mysqli_query($con,$sql3);
-      while($prices=mysqli_fetch_array($result4)) 
-      { 
-        $price= $prices[0];
-      }
+    // $sql3="SELECT price FROM products_last  where sku='".$row['sku']."' and segment='LOYALTY_CON' limit 1";
+    // $result4=mysqli_query($con,$sql3);
+    //   while($prices=mysqli_fetch_array($result4)) 
+    //   { 
+    //     $price= $prices[0];
+    //   }
   
-    $posts['price']=$price;
-*/
+    // $posts['price']=$price;
+
 
   array_push($response, $posts);
 
