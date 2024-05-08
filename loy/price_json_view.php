58a6cb5bcd87e0f1b4564f639e011b0b4c19577a
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2024-05-09 00:47:19 +1000

    update rrp

--- loy/price_json_view.php
+++ loy/price_json_view.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');   
 include 'data/db_connection.php'; 
-$sql="select distinct T1.sku,T2.name,T2.category,T3.rrp,T1.vpp,T1.price from products_new_pricing as T1 inner join products_last as T2 on T1.sku=T2.sku inner join price_tiers as T3 on T2.sku=T3.sku;";
+$sql="select distinct T1.sku,T2.name,T2.category,T3.rrp,T1.vpp,T3.top_tier,T1.price from products_new_pricing as T1 inner join products_last as T2 on T1.sku=T2.sku inner join price_tiers as T3 on T2.sku=T3.sku;";
 $response = array();
 $posts = array();
 $result=mysqli_query($con,$sql);
@@ -13,6 +13,7 @@ while($row=mysqli_fetch_array($result))
   $posts['category']=$row['category'];
   $posts['rrp']=$row['rrp'];
   $posts['vpp']=$row['vpp'];
+  $posts['topoints']=$row['top_tier'];
   $posts['price']=$row['price']; 
   array_push($response, $posts);
 
