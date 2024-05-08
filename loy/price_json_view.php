c47f04c301c64c77f20fe9d890c8fcdefc4d867c
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2024-05-09 00:55:12 +1000

    update rrp vpp decimals view

--- loy/price_json_view.php
+++ loy/price_json_view.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');   
 include 'data/db_connection.php'; 
-$sql="select distinct T1.sku,T2.name,T2.category,T3.rrp,T1.vpp,T3.top_tier,T1.price from products_new_pricing as T1 inner join products_last as T2 on T1.sku=T2.sku inner join price_tiers as T3 on T2.sku=T3.sku;";
+$sql="select distinct T1.sku,T2.name,T2.category,T3.rrp,T3.vpp,T3.top_tier,T1.price from products_new_pricing as T1 inner join products_last as T2 on T1.sku=T2.sku inner join price_tiers as T3 on T2.sku=T3.sku;";
 $response = array();
 $posts = array();
 $result=mysqli_query($con,$sql);
