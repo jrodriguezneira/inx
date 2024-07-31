0c698ba8f47fec8db4b89d2fcec4b8fc9a77602a
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2024-07-31 17:41:55 +1000

    Update RO

--- loy/current_price_json_view.php
+++ loy/current_price_json_view.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');   
 include 'data/db_connection.php'; 
 $sql="select distinct sku,name,rrp, substring_index(substring(SUBSTRING_INDEX(price,',', 1),3),'-',1) as Top_Points,
-substring_index((substring_index((substring(price,-10)),'-',-1)),\"'\",1) as Top_Pay,price,offer 
+substring_index((substring_index((substring(price,-10)),'-',-1)),\"'\",1) as Top_Pay,price,offer,ro 
 from products_last ;";
 $response = array();
 $posts = array();
@@ -16,6 +16,7 @@ while($row=mysqli_fetch_array($result))
   $posts['top']=$row['Top_Points'];
   $posts['pay']=$row['Top_Pay'];
   $posts['offer']=$row['offer'];
+  $posts['ro']=$row['ro'];
   if(intval($posts['rrp'])==intval($posts['pay'])){
     $check="Aligned";
     }else{
@@ -27,7 +28,16 @@ while($row=mysqli_fetch_array($result))
   if($posts['pay']==0){
     $check="Points Only";
   }
-  $posts['check']=$check; 
+  $posts['check']=$check;
+  
+  if($posts['ro']=="[12,24]"){
+    $ro="RO";
+  }
+  if($posts['ro']=="[]"){
+    $ro="Outright";
+  }
+  $posts['ro']=$ro;
+
   $posts['price']=$row['price']; 
   array_push($response, $posts);
 
