0b8bf7fbd9d56e00ca37000c20f35666bdfc9dc1
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2025-08-21 17:32:21 +1000

    Stats

---
+++ loy/pro_stats.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');   
+include 'data/db_connection.php'; 
+$sql_stats="select
+(select count(sku) from products_last where segment='LOYALTY_CON') as prod,
+(select count(sku) from products_last where offer = 'Hot Offer' and segment='LOYALTY_CON') as off,
+(select count(sku) from products_last where ro = '[]' and segment='LOYALTY_CON') as outright,
+(select count(sku) from products_last where ro <> '[]' and segment='LOYALTY_CON') as ro";
+
+$response = array();
+$posts = array();
+$result=mysqli_query($con,$sql);
+while($row=mysqli_fetch_array($result)) 
+{ 
+  
+  $posts['prod']=$row['prod'];
+  $posts['off']=$row['off'];
+  $posts['outright']=$row['outright'];
+  $posts['ro']=$row['ro'];
+  array_push($response, $posts);
+
+  
+
+} 
+echo "{\"data\": ".json_encode($response)."}"; 
+//$file = 'file.json';
+//file_put_contents($file, $json);
+?>
