6fff60ec2e60c41955929d28a034d3313f467f6e
JR <metaleyesair@hotmail.com>
2025-08-26 02:52:45 +1000

    Pro_qty_mon

---
+++ loy/pro_qty_mon.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json');   
+// Allow requests from React frontend (adjust domain/port as needed)
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+include 'data/db_connection.php'; 
+$sql="select total,months from products_x_month;";
+
+$response = array();
+$posts = array();
+$result=mysqli_query($con,$sql);
+while($row=mysqli_fetch_array($result)) 
+{ 
+  
+  $posts['total']=$row['total'];
+  $posts['months']=$row['months'];
+  array_push($response, $posts);
+} 
+echo "{\"data\": ".json_encode($response)."}"; 
+//$file = 'file.json';
+//file_put_contents($file, $json);
+?>
