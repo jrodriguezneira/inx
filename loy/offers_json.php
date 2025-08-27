709082a5224e03a22c476c529697000229ce72cd
JR <metaleyesair@hotmail.com>
2025-08-28 01:37:22 +1000

    Offers

--- loy/offers_json.php
+++ loy/offers_json.php
@@ -1,5 +1,8 @@
 <?php
-header('Content-Type: application/json');   
+header('Content-Type: application/json');  
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");   
 include 'data/db_connection.php'; 
 $sql="select sku,name,category,offer,stock,price from products_last where offer ='Hot Offer' and segment='LOYALTY_CON' order by category,name;";
 $response = array();
