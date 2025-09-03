0883f3344d24672fd7db9a07253c27890dc230f7
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2025-09-04 01:39:13 +1000

    Data Port 5

--- loy/pro_qty_mon.php
+++ loy/pro_qty_mon.php
@@ -1,10 +1,16 @@
 <?php
 header('Content-Type: application/json');   
 // Allow requests from React frontend (adjust domain/port as needed)
-header("Access-Control-Allow-Origin: http://10.25.30.23:30080");
-header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+$allowed_origins = [
+    "http://localhost:3000",
+    "http://10.25.30.23:30080"
+];
+
+if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
 
 include 'data/db_connection.php'; 
 $sql="select total,months from products_x_month;";
