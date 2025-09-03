c595bbfabe7cd2e7f039307dd83b10795669c8fd
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2025-09-04 01:10:58 +1000

    Data Port

--- loy/pro_stats.php
+++ loy/pro_stats.php
@@ -2,6 +2,7 @@
 header('Content-Type: application/json');   
 // Allow requests from React frontend (adjust domain/port as needed)
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://localhost:30080");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
