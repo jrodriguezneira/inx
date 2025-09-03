ac1db61404d4ecf20752495f9cf1b1b1dfddab47
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2025-09-04 01:47:17 +1000

    Data Port Fix

--- loy/offers_json.php
+++ loy/offers_json.php
@@ -1,6 +1,13 @@
 <?php
 header('Content-Type: application/json');  
-header("Access-Control-Allow-Origin: http://localhost:3000");
+$allowed_origins = [
+    "http://localhost:3000",
+    "http://10.25.30.23:30080"
+];
+
+if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");   
 include 'data/db_connection.php'; 
